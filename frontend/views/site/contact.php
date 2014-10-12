14bafaaad82d60a701f3f2232daa09a814de4144
Vladislav Shumilov <vladislav.shumilov@gmail.com>
2014-10-12 16:01:43 +0600

    understood how to change titles

---
+++ frontend/views/site/contact.php
@@ -0,0 +1,37 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\captcha\Captcha;
+
+/* @var $this yii\web\View */
+/* @var $form yii\widgets\ActiveForm */
+/* @var $model \frontend\models\ContactForm */
+
+$this->title = 'Поддержка';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-contact">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        Если у Вас есть деловые предложения или другие вопросы, пожалуйста заполните приведенную ниже форму. Спасибо.
+    </p>
+
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
+                <?= $form->field($model, 'name') ?>
+                <?= $form->field($model, 'email') ?>
+                <?= $form->field($model, 'subject') ?>
+                <?= $form->field($model, 'body')->textArea(['rows' => 6]) ?>
+                <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+                    'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+                ]) ?>
+                <div class="form-group">
+                    <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+                </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+
+</div>
---
+++ frontend/views/site/login.php
@@ -0,0 +1,32 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $form yii\widgets\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+$this->title = 'Вход';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-login">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Для того, чтобы войти на сайт, заполните все поля:</p>
+
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+                <?= $form->field($model, 'username') ?>
+                <?= $form->field($model, 'password')->passwordInput() ?>
+                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                <div style="color:#999;margin:1em 0">
+                    Если Вы забыли свой пароль, то Вы можете <?= Html::a('сбросить его', ['site/request-password-reset']) ?>.
+                </div>
+                <div class="form-group">
+                    <?= Html::submitButton('Войти', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+</div>
